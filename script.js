5dc86fbba4606e431e8ff72bc7e5dc767a565a1c
Dias <65382116+diasgt123@users.noreply.github.com>
2022-10-04 18:12:34 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-setTimeout(() => {
-    const mypic = document.getElementById('mypic');
-    mypic.style.display = 'none';
-}, 3000);
-$(function () {
-    $(document).scroll(function () {
-      var $nav = $(".heading-fixed-top");
-      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    });
-  });
-
-  window.addEventListener('DOMContentLoaded', (event) => {
-    jQuery('div#main-nav.navbar').css("background-color", "rgba(255, 255, 255, 0.2)");
-    jQuery(window).scroll(function() {
-        if (jQuery(window).scrollTop() > 0) {
-            jQuery('div#main-nav.navbar').css("background-color", "#000");              
-        } else {
-            jQuery('div#main-nav.navbar').css("background-color", "rgba(255, 255, 255, 0.2)");
-        }
-    });
-}); 
+setTimeout(function() {
+    document.getElementById('con').style.visibility='none'
+  }, 1000);

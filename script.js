b85d4489622e8804da239d49ce8652172a8047ea
Dias <65382116+diasgt123@users.noreply.github.com>
2022-10-07 23:59:12 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-setTimeout(function() {
-    document.getElementById('con').style.visibility='none'
-  }, 1000);
+
+  // setInterval(function(){
+  //   document.getElementById('con').style.scale='0.6';
+  //   document.getElementById('blonde').style.position='-20px';
+
+  // },3000);
+  setInterval(function(){
+    document.getElementById('mypic').style.scale='0'
+
+  },2000);
